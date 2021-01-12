9788ff5aa7ab6e94c656f94f6dabe497a96b85f6
Rin <ry00001@protonmail.com>
2021-01-12 19:37:49 +0000

    add console explanation text

--- scenes/default.lua
+++ scenes/default.lua
@@ -8,6 +8,8 @@ return LightningActor {
             L.cprint('h')
         end)
 
+        UILabelActor(100, 200, string.format('You can open the LFW console by pressing [%s]!', string.upper(_CONSOLE_KEY)))
+
         CursorActor()
     end
 }
